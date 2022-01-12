d5a08e7c636d70ada10c36a812a14eae3ab456b5
ZEEE <j1nd2su@gmail.com>
2022-01-13 00:03:23 +0900

    chore: beatmap component structuring

--- src/Components/Beatmap.js
+++ src/Components/Beatmap.js
@@ -1,15 +1,74 @@
 import React, { Fragment, } from "react"
+import { Tooltip } from 'antd'
+
+const convertRankedStatusToText = (ranked) => {
+    var temp
+    switch (ranked) {
+        default:
+            temp = "UNKNOWN"
+            break
+        case -2:
+            temp = "GRAVEYARD"
+            break
+        case -1:
+            temp = "WIP"
+            break
+        case 0:
+            temp = "PENDING"
+            break
+        case 1:
+            temp = "RANKED"
+            break
+        case 2:
+            temp = "APPROVED"
+            break
+        case 3:
+            temp = "QUALIFIED"
+            break
+        case 4:
+            temp = "LOVED"
+            break
+    }
+    return temp
+}
 
 function Beatmap({ bmap }) {
     return (
         <Fragment>
             <div className="beatmap-block">
                 <div className="beatmap-single">
-                    <div className="beatmap-bg-default"></div>
                     <a href="/" target="_blank" id={bmap.id} className="card-header"
-                    style={{backgroundImage: 'url(/images/beatmaps-default.png), linear-gradient(to right, #00000099, #ffe4e100), url(https://assets.ppy.sh/beatmaps/'+bmap.id+'/covers/cover.jpg?1622784772'}}>
+                    style={{backgroundImage: "url(https://assets.ppy.sh/beatmaps/" + bmap.id + "/covers/cover.jpg?1622784772, linear-gradient(to right, #00000099, #ffe4e100), url(/images/beatmaps-default.png)"}}>
                         <div className="card-header-beatmapinfo">
+                            <ul>
+                                <li>
+                                    <div className={"ranked-status " + convertRankedStatusToText(bmap.ranked)}>
+                                        {convertRankedStatusToText(bmap.ranked)}
+                                    </div>
+                                    {bmap.nsfw &&
+                                        <div className={"nsfw"}>
+                                            EXPLICIT
+                                        </div>
+                                    }
+                                </li>
+                                <li>
+                                    <div className="card-haeder-stats">
+                                        <Tooltip placement="top" title={"Favorites count: " + bmap.favourite_count}>
+                                            <i class="fas fa-heart"></i> {bmap.favourite_count}
+                                        </Tooltip>
+                                        <Tooltip placement="top" title={"Play count: " + bmap.play_count}>
+                                            <i class="fas fa-play-circle"></i> {bmap.play_count}
+                                        </Tooltip>
+                                        <Tooltip placement="top" title={"BPM: " + bmap.beatmaps.length}>
+                                            <i class="fas fa-clipboard-list"></i> {bmap.beatmaps.length}
+                                        </Tooltip>
+                                    </div>
+                                </li>
+                            </ul>
                             {bmap.id} - {bmap.title}
+                        </div>
+                        <div>
+
                         </div>
                     </a>
                 </div>
--- src/Pages/Beatmaps.js
+++ src/Pages/Beatmaps.js
@@ -50,7 +50,7 @@ function Beatmaps() {
 
   const scrollHandler = () => {
     const documentData = document.documentElement;
-    if (documentData.scrollTop + documentData.clientHeight + 350 >= documentData.scrollHeight && !Loading) {
+    if (documentData.scrollTop + documentData.clientHeight + 500 >= documentData.scrollHeight && !Loading) {
       getData()
     }
   }
